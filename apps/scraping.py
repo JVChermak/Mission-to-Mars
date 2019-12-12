d272c998d57c6b5e60fff51a8089332d4f529f80
Jasmin Chermak <jasminchermak@Jasmins-MacBook-Air.local>
2019-12-11 22:54:22 -0800

    Added functions and exceptions.

--- apps/scraping.py
+++ apps/scraping.py
@@ -3,67 +3,88 @@ from splinter import Browser
 from bs4 import BeautifulSoup
 import pandas as pd
 
-# Set executable path and initialize the chrome browser in splinter
-executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
-browser = Browser('chrome', **executable_path)
+def mars_news(browser):
+    # Visit the Mars NASA news site
+    url = 'https://mars.nasa.gov/news/'
+    browser.visit(url)
+    # Optional delay for loading the page
+    browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
 
-# Visit the Mars NASA new site
-url = 'https://mars.nasa.gov/news/'
-browser.visit(url)
-# Optional delay for loading the page
-browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
+    # Set up the HTML parser
+    html = browser.html
+    news_soup = BeautifulSoup(html, 'html.parser')
+    # Add try/except for error handling
+    try:
+        slide_elem = news_soup.select_one('ul.item_list li.slide')
+        # Use the parent element to find teh first 'a' tag and save it as news_title
+        news_title = slide_elem.find('div', class_='content_title').get_text()
+        # Use the parent element to find the paragraph text
+        news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+    except AttributeError:
+        return None, None
 
-# Set up the HTML parser
-html = browser.html
-news_soup = BeautifulSoup(html, 'html.parser')
-slide_elem = news_soup.select_one('ul.item_list li.slide')
-
-# Scrape page for most recent article
-slide_elem.find('div', class_='content_title')
-
-# Use the parent element to find teh first 'a' tag and save it as news_title
-news_title = slide_elem.find('div', class_='content_title').get_text()
-news_title
-
-# Use the parent element to find the paragraph text
-news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
-news_p
+    return news_title, news_p
 
 # Featured Images
+def featured_image(browser):
+    # Visit URL
+    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser.visit(url)
 
-# Visit URL
-url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
-browser.visit(url)
+    # Find and click the full image button
+    full_image_elem = browser.find_by_id('full_image')
+    full_image_elem.click()
 
-# Find and click the full image button
-full_image_elem = browser.find_by_id('full_image')
-full_image_elem.click()
+    # Find the more info button and click that
+    browser.is_element_present_by_text('more info', wait_time=1)
+    more_info_elem = browser.find_link_by_partial_text('more info')
+    more_info_elem.click()
 
-# Find the more info button and click that
-browser.is_element_present_by_text('more info', wait_time=1)
-more_info_elem = browser.find_link_by_partial_text('more info')
-more_info_elem.click()
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = BeautifulSoup(html, 'html.parser')
+    try:
+        # Find the relative image url
+        img_url_rel = img_soup.select_one('figure.lede a img').get("src")
+    except AttributeError:
+        return None
+    # Use the base URL to create an absolute URL
+    img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
+    return img_url
 
-# Parse the resulting html with soup
-html = browser.html
-img_soup = BeautifulSoup(html, 'html.parser')
+def mars_facts():
+    try:
+        # Create a Data Frame from the Mars Facts table
+        df = pd.read_html('http://space-facts.com/mars/')[0]
+    except BaseException:
+        return None
+    # Assign columns and set index of Data Frame
+    df.columns=['description', 'value']
+    df.set_index('description', inplace=True)
+    # Convert the DataFrame into html
+    return df.to_html()
 
-# Find the relative image url
-img_url_rel = img_soup.select_one('figure.lede a img').get("src")
-img_url_rel
+def scrape_all():
+    import datetime as dt
 
-# Use the base URL to create an absolute URL
-img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
-img_url
+    # Set executable path and initialize the chrome browser in splinter
+    browser = Browser('chrome', executable_path='/usr/local/bin/chromedriver', headless=True)
 
-# Create a Data Frame from the Mars Facts table
-df = pd.read_html('http://space-facts.com/mars/')[0]
-df.columns=['description', 'value']
-df.set_index('description', inplace=True)
-df
+    # Set news title and paragraph variables
+    news_title, news_paragraph = mars_news(browser)
 
-# Convert the DataFrame into html
-df.to_html()
+    # Run all scraping functions and store results in dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+    # Turn off automated browsing session
+    browser.quit()
+    return data
 
-# Turn off automated browsing session
-browser.quit()
+if __name__ == "__main__":
+    # If running as script, print scraped data
+    print(scrape_all())
